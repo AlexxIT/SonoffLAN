4931132e54d8ad80a9f36f10f718ff588a2033de
Masterz69 <igors.micko@gmail.com>
2022-09-02 18:35:13 +0300

    THR316D - humidity with decimals
    TH16R2 show in App only round numbers for humidity, but THR316D show with one decimal sign.
    May be more sophisticated, like model-specific, logic can be implemented - with parameter to specify decimal signs count in output.
    But for now pls not drop decimal in THR316D readings.

--- custom_components/sonoff/sensor.py
+++ custom_components/sonoff/sensor.py
@@ -152,8 +152,7 @@ class XHumidityTH(XSensor):
     def set_state(self, params: dict = None, value: float = None):
         try:
             value = params.get("currentHumidity") or params["humidity"]
-            # we need integer, because cloud always int, and local not
-            value = round(float(value))
+            value = float(value)
             # filter zero values
             # https://github.com/AlexxIT/SonoffLAN/issues/110
             if value != 0:
