d9227bb637941cbed013b0b9f1082a1044ea4161
Alexey Khit <alexey.khit@gmail.com>
2022-12-30 13:56:32 +0300

    Add Mini R3 model

--- custom_components/sonoff/core/devices.py
+++ custom_components/sonoff/core/devices.py
@@ -270,6 +270,8 @@ DEVICES = {
     # https://github.com/AlexxIT/SonoffLAN/issues/766
     136: [XLightB05B, RSSI],  # Sonoff B05-BL
     137: [XLightL1, RSSI],
+    # https://github.com/AlexxIT/SonoffLAN/issues/623#issuecomment-1365841454
+    138: SPEC_1CH,  # MINIR3
     # https://github.com/AlexxIT/SonoffLAN/issues/808
     154: [XWiFiDoor, Battery, RSSI],  # DW2-Wi-Fi-L
     162: SPEC_3CH,  # https://github.com/AlexxIT/SonoffLAN/issues/659
