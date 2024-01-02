5d305aad9161362d22b311c6326f99a2aaa7c767
Hmmbob <33529490+hmmbob@users.noreply.github.com>
2024-01-02 14:46:06 +0100

    Update backward compatibility page

--- custom_components/sonoff/core/backward.py
+++ custom_components/sonoff/core/backward.py
@@ -1,4 +1,4 @@
-"""Why Hass v2022.5 minimal?
+"""Why Hass v2023.1 minimal?
 
 - v2021.7 - new Entity attributes style
 - v2021.8 - new electric unit_of_measurement
@@ -8,7 +8,9 @@
 - v2021.12 - new FanEntity percentage logic
 - v2021.12 - new SensorDeviceClass, SensorStateClass classes
 - v2022.5 - new LightEntity ColorMode and LightEntityFeature
+- v2022.11 - new UnitOfEnergy, UnitOfPower, UnitOfTemperature
+- V2023.1 - new UnitOfElectricCurrent, UnitOfElectricPotential
 """
 from homeassistant.const import MAJOR_VERSION, MINOR_VERSION
 
-hass_version_supported = (MAJOR_VERSION, MINOR_VERSION) >= (2022, 5)
+hass_version_supported = (MAJOR_VERSION, MINOR_VERSION) >= (2023, 1)
