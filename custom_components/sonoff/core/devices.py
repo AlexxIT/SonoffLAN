1309b77722a139c8783bc500829108e3972c6767
Alex X <alexey.khit@gmail.com>
2024-01-04 21:46:05 +0300

    Fix zigbee light spec

--- custom_components/sonoff/core/devices.py
+++ custom_components/sonoff/core/devices.py
@@ -350,7 +350,7 @@ DEVICES = {
     212: [Switch1, Switch2, Switch3, Switch4, XT5Light, XT5Action],  # T5-4C-86
     1000: [XRemoteButton, Battery],  # zigbee_ON_OFF_SWITCH_1000
     1256: [spec(XSwitch, base="light")],  # ZCL_HA_DEVICEID_ON_OFF_LIGHT
-    1257: [spec(XLightD1, base="light")],  # ZigbeeWhiteLight
+    1257: [XLightD1],  # ZigbeeWhiteLight
     # https://github.com/AlexxIT/SonoffLAN/issues/972
     1514: [XZigbeeCover, spec(XSensor, param="battery", multiply=2)],
     1770: [
