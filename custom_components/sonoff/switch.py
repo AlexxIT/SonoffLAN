7a9aea14b031c9ba94ceadf6f641b0eeebf3e917
Alex X <alexey.khit@gmail.com>
2024-02-13 08:06:11 +0300

    Fix custom device_class fan support after Hass v2023.12 #1266

--- custom_components/sonoff/switch.py
+++ custom_components/sonoff/switch.py
@@ -22,7 +22,7 @@ class XSwitch(XEntity, SwitchEntity):
     def set_state(self, params: dict):
         self._attr_is_on = params["switch"] == "on"
 
-    async def async_turn_on(self, **kwargs):
+    async def async_turn_on(self, *args, **kwargs):
         await self.ewelink.send(self.device, {"switch": "on"})
 
     async def async_turn_off(self):
