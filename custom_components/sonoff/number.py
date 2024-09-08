d90288de7c25708280b448f891bf12e85a623a7c
wareczek <wareczek@gmail.com>
2024-09-08 23:58:31 +0200

    fixed pulseWidth disabled by default

--- custom_components/sonoff/number.py
+++ custom_components/sonoff/number.py
@@ -38,7 +38,7 @@ class XNumber(XEntity, NumberEntity):
 class XPulseWidth(XNumber):
     param = "pulseWidth"
 
-    __attr_entity_registry_enabled_default = False
+    _attr_entity_registry_enabled_default = False
 
     _attr_native_max_value = 36000
     _attr_native_min_value = 0.5
