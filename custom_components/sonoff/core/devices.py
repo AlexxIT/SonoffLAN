547cd07a172f20daf447929357e93a2aa0efe665
Alex X <alexey.khit@gmail.com>
2026-09-09 17:27:37 +0300

    Add support Sonoff Hydro Duo (SWV-ZF2E) #1859

--- custom_components/sonoff/core/devices.py
+++ custom_components/sonoff/core/devices.py
@@ -788,6 +788,13 @@ DEVICES = {
     7039: [XButtonKey, Battery, ZRSSI],
     # SWV-ZNE, https://github.com/AlexxIT/SonoffLAN/issues/1814
     7047: [spec(XBoolSwitch, param="switch_00", uid="switch"), Battery, ZRSSI],
+    # SWV-ZF2E https://github.com/AlexxIT/SonoffLAN/issues/1859
+    7048: [
+        spec(XToggle, param="switch_00", uid="1"),
+        spec(XToggle, param="switch_01", uid="2"),
+        Battery,
+        ZRSSI,
+    ],
     # SNZB-06P24 https://github.com/AlexxIT/SonoffLAN/issues/1852
     7051: [XHumanSensor, spec(XSensor, param="illumination", uid="illuminance")],
     # SNZB-02B
